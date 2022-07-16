e18f5a230ac0f97d2875a948198a913b36b3f43e
Bnh31 <zimarika@protonmail.com>
2022-07-16 15:40:08 +0100

    hello

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import {Routes,Route} from 'react-router-dom'
 import Home from './routes/home.component';
+import {Fragment} from 'react'
+import {Link,Outlet} from 'react-router-dom'
 import Navigation from './routes/navigation.component';
+const Shop =()=>{
+  return(
+    <Fragment>
+      <Outlet/>
+      <h1>you are in the shop route</h1>
+      <Link to='/'>go to home page</Link>
+    </Fragment>
 
+  )
+}
 
 const App = () => {
   return ( 
@@ -9,6 +20,10 @@ const App = () => {
       <Route path='/' element={<Home/>}>
         <Route path='' element={<Navigation/>}/>
       </Route>
+      <Route path='/shop' element={<Shop/>}>
+        <Route path='' element={<Navigation/>}/>
+      </Route>
+
     </Routes>
 
   )
